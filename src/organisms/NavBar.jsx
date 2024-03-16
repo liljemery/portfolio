ec19ba65f6094f7ad79992fa3f850e13654a7d85
“JEREMY <Jeremyinoa67@gmail.com>
2024-03-16 19:14:24 -0400

    fix: adds flowbite js script

--- src/organisms/NavBar.jsx
+++ src/organisms/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
     return (
     <>
-        <nav className="bg-whitefixed w-full z-20 top-0 start-0 border-b border-gray-200">
+        <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ms-auto">
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
